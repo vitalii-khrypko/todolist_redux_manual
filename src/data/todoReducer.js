3a0a6fa32c4b1e0c13322acf212f7152b8287688
Vitalii Khrypko <00hripkovitaliy00@gmail.com>
2025-02-02 20:41:41 +0200

    Delete btn added, Todo.jsx & todoReducer.js ref

--- src/data/todoReducer.js
+++ src/data/todoReducer.js
@@ -1,37 +1,44 @@
 const ADD_TODO = "ADD-TODO";
 const NEW_TODO_TEXT = "NEW-TODO-TEXT";
+const DELETE_TODO = "DELETE-TODO";
 
 const todoReducer = (state, action) => {
     switch(action.type) {
         case ADD_TODO:
-            // Finding max id and adding 1
             const maxId = state.todos.reduce((max, todo) => Math.max(max, todo.id), 0);
             let newTodo = {
                 id: maxId + 1,
                 todoText: state.newTodoText
             };
-            state.todos.push(newTodo)
-            state.newTodoText = "";
-            return state;
+            return {
+                ...state,
+                todos: [...state.todos, newTodo],
+                newTodoText: ""
+            };
+
         case NEW_TODO_TEXT:
-            state.newTodoText = action.text;
-            return state;
+            return {
+                ...state,
+                newTodoText: action.text
+            };
+
+        case DELETE_TODO:
+            return {
+                ...state,
+                todos: state.todos.filter(todo => todo.id !== action.todoId)
+            };
+
         default:
             return state;
     }
 }
 
-export const addTodoActionCreator = () => {
-    return {
-        type: ADD_TODO
-    }
-}
+export const addTodoActionCreator = () => (
+    { type: ADD_TODO }
+);
 
-export const newTodoTextActionCreator = (text) => {
-    return {
-        type: NEW_TODO_TEXT,
-        text
-    }
-}
+export const newTodoTextActionCreator = (text) => ({ type: NEW_TODO_TEXT, text });
+
+export const deleteTodoActionCreator = (todoId) => ({ type: DELETE_TODO, todoId });
 
 export default todoReducer;
